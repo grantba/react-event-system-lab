deba33fb9e64d81437c0319eb5baecf447d50e7c
Blaire <grant.blaire@gmail.com>
2021-07-21 14:28:56 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,32 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+    render() {
+        return (
+            <div id="eyes-on-me">
+                <button onFocus={(e) => console.log('Good!')} onBlur={(e) => console.log('Hey! Eyes on me!')}>Button</button>
+            </div>
+        )
+    }
+}
+
+// import React from 'react';
+
+// class EyesOnMe extends React.Component {
+
+//   onFocus = () => console.log('Good!')
+
+//   onBlur = () => console.log('Hey! Eyes on me!')
+
+//   render() {
+//     return (
+//       <button onFocus={this.onFocus} onBlur={this.onBlur}>
+//         Eyes on me, please!
+//       </button>
+//     )
+//   }
+// }
+
+// export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,33 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+    render() {
+        return (
+            <div id="keypad">
+                <input type="password" name="password" onKeyUp={(e) => console.log('Entering password...')} placeholder='Enter Password' />
+            </div>
+        )
+    }
+}
+
+// import React from 'react';
+
+// class Keypad extends React.Component {
+
+//   handleInputPassword = () => console.log('Entering password...')
+
+//   render() {
+//     return (
+//       <div>
+//         <input 
+//           type="password"
+//           onKeyUp={this.handleInputPassword} 
+//         />
+//       </div>
+//     )
+//   }
+// }
+
+// export default Keypad;
